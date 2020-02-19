83387df9b18940548a43346c6363126b8761d9a7
val44087 <61064107+val44087@users.noreply.github.com>
2020-02-19 19:42:47 +0000

    Day 3 Game

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,40 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+
+const secretNumber = 6;
+
+const squares = document.querySelectorAll('.square') as NodeListOf<HTMLDivElement>;
+
+let currentSquare = 1;
+
+squares.forEach(sq => {
+    if (currentSquare === secretNumber) {
+        sq.dataset.secret = 'true';
+    }
+    currentSquare++;
+    sq.addEventListener('click', handleClick);
+});
+
+function handleClick() {
+    const that = this as HTMLDivElement;
+
+    // console.log('Got Clicked', that);
+    if (that.dataset.secret) {
+        that.classList.add('winner');
+        that.removeEventListener('click', handleClick);
+
+        squares.forEach(sq => {
+            if (sq !== that) {
+                that.classList.add('loser');
+            }
+
+        });
+    } else {
+        that.classList.add('loser');
+        that.removeEventListener('click', handleClick);
+    }
+}
+
+
+
 
